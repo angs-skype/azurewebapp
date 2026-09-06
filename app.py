f0e0c335bda2ec623ec84d46148ccd8142be1c81
angs-skype <144552488+angs-skype@users.noreply.github.com>
2026-09-06 15:26:35 +0000

    test1

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+import sys
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    # Demonstrating simple output using Python 3.14
+    version = f"{sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro}"
+    return f"<h1>Hello World!</h1><p>Your Python web app is running smoothly on version <b>{version}</b>.</p>"
+
+if __name__ == "__main__":
+    app.run()
